b6ebc5dcf086501cd223c4818d7dbdc8b399a721
Syed Ali Hasan Moosavi <shalimoosavi@gmail.com>
2026-05-01 17:33:15 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+import ThreeBG from "./components/ThreeBG";
+
+export default function App() {
+  return (
+    <>
+      <ThreeBG />
+      <Hero />
+    </>
+  );
+}
